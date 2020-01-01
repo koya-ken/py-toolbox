13a4f27c0892828d2c2870485e90bc8a79ddb7a3
koya-ken <koya.smp@gmail.com>
2020-01-02 01:49:13 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name="py-toolbox",
+setup(name="py_toolbox",
       version="0.1",
       description="python script utility",
       url="https://github.com/koya-ken/py-toolbox",
